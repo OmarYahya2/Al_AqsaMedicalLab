06cc77f8105c75689f661d6abb7057a8e86b0beb
OmarYahya2 <omar.yahya965@gmail.com>
2025-08-21 21:42:04 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // أي طلب يبدأ بـ /api
+        destination: "https://al-aqsabackend-uokt.onrender.com/:path*", // ضع هنا رابط backend على Render
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
