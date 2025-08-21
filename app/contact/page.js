1532f7bf2bf81d06dae7705ff560be0e68d8f031
OmarYahya2 <omar.yahya965@gmail.com>
2025-08-21 22:16:36 +0300

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Navigation from "@/components/navigation";
 import Footer from "@/components/footer";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -15,8 +17,37 @@ import {
   NavigationIcon,
   Building,
 } from "lucide-react";
-
+import ContactForm from "./ContactForm";
 export default function ContactPage() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = {
+      first_name: formData.get("firstName"),
+      last_name: formData.get("lastName"),
+      email: formData.get("email"),
+      phone: formData.get("phone"),
+      subject: formData.get("subject"),
+      message: formData.get("message"),
+    };
+
+    const res = await fetch(
+      "https://al-aqsabackend-uokt.onrender.com/api/contact/",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      }
+    );
+
+    if (!res.ok) {
+      console.error("Error sending data to backend:", await res.text());
+    } else {
+      alert("تم الاستلام! سنرد عليك في أقرب وقت ممكن");
+    }
+  }
+
   return (
     <div className="min-h-screen bg-background">
       <Navigation />
