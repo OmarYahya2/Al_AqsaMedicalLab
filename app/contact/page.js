0f66caabcb07e8447305b65886047011d818b949
OmarYahya2 <omar.yahya965@gmail.com>
2025-08-21 22:34:50 +0300

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -19,35 +19,6 @@ import {
 } from "lucide-react";
 import ContactForm from "./ContactForm";
 export default function ContactPage() {
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    const formData = new FormData(e.target);
-    const data = {
-      first_name: formData.get("firstName"),
-      last_name: formData.get("lastName"),
-      email: formData.get("email"),
-      phone: formData.get("phone"),
-      subject: formData.get("subject"),
-      message: formData.get("message"),
-    };
-
-    const res = await fetch(
-      "https://al-aqsabackend-uokt.onrender.com/api/contact/",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-      }
-    );
-
-    if (!res.ok) {
-      console.error("Error sending data to backend:", await res.text());
-    } else {
-      alert("تم الاستلام! سنرد عليك في أقرب وقت ممكن");
-    }
-  }
-
   return (
     <div className="min-h-screen bg-background">
       <Navigation />
@@ -297,6 +268,7 @@ export default function ContactPage() {
                         إرسال الرسالة
                       </Button>
                     </form>
+                    <ContactForm />
                   </CardContent>
                 </Card>
               </div>
